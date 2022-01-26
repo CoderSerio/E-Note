7235ba9a9386ea4fa2b153493a48bf3de591a544
Tosuke <1848228664@qq.com>
2022-01-27 01:56:21 +0800

    1.0.2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 Vue.config.productionTip = false
+
+Vue.use(MintUI)
+
+
 
 new Vue({
   router,
