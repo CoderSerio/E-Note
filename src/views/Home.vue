ec29e08fd7acdd6f34b48de6401824b63ea67278
Serio <2779066456@qq.com>
2022-01-27 02:22:27 +0800

    删除部分文件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-<hello></hello>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import hello from'../components/hello.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
-	hello
+    
   }
 }
 </script>
